bfebd08751f930889bb82645032beb2ea362b84e
nikhea <imonikheaugbodaga@gmail.com>
2024-10-16 11:37:16 +0100

    wip: rabbit mq

---
+++ app/api/v1/swagger/route.ts
@@ -0,0 +1,28 @@
+// import { NextApiRequest, NextApiResponse } from "next";
+// import swaggerJsdoc from "swagger-jsdoc";
+
+// const options: swaggerJsdoc.Options = {
+//   definition: {
+//     openapi: "3.0.0",
+//     info: {
+//       title: "Next.js Swagger API",
+//       version: "1.0.0",
+//       description: "A sample API for learning Swagger and Next.js",
+//     },
+//     servers: [
+//       {
+//         url: 'http://localhost:3000/api',
+//       },
+//     ],
+//   },
+//   apis: ["./pages/api/*.ts", "./app/api/*.ts"],
+// };
+
+// const swaggerSpec = swaggerJsdoc(options);
+
+// function swagger(req: NextApiRequest, res: NextApiResponse) {
+//   res.setHeader("Content-Type", "application/json");
+//   res.send(swaggerSpec);
+// }
+
+// export default swagger;
