9a699331db25b0724c5f5f3333fb1c57849f9dc5
nikhea <imonikheaugbodaga@gmail.com>
2024-10-16 11:38:19 +0100

    wip: rabbit mq

--- scripts/emailWorker.ts
+++ scripts/emailWorker.ts
@@ -1,44 +1,44 @@
-require("dotenv").config();
-const amqplib = require("amqplib");
+// require("dotenv").config();
+// const amqplib = require("amqplib");
 
-let channel: any;
+// let channel: any;
 
-const RABBITMQ_URL = process.env.RABBITMQ_URL!;
-const RABBITMQ_QUEUE = process.env.RABBITMQ_QUEUE!;
+// const RABBITMQ_URL = process.env.RABBITMQ_URL!;
+// const RABBITMQ_QUEUE = process.env.RABBITMQ_QUEUE!;
 
-async function connectToRabbitMQConsumer(): Promise<any> {
-  if (channel) return channel;
+// async function connectToRabbitMQConsumer(): Promise<any> {
+//   if (channel) return channel;
 
-  const connection = await amqplib.connect(RABBITMQ_URL);
-  channel = await connection.createChannel();
-  await channel.assertQueue(RABBITMQ_QUEUE, { durable: true });
+//   const connection = await amqplib.connect(RABBITMQ_URL);
+//   channel = await connection.createChannel();
+//   await channel.assertQueue(RABBITMQ_QUEUE, { durable: true });
 
-  console.log("Connected to RabbitMQ and Queue asserted.");
-  return channel;
-}
+//   console.log("Connected to RabbitMQ and Queue asserted.");
+//   return channel;
+// }
 
 async function consumeEmails() {
   try {
-    const channel = await connectToRabbitMQConsumer();
-    channel.consume(
-      RABBITMQ_QUEUE,
-      async (msg: any) => {
-        if (msg !== null) {
-          try {
-            const emailTask = JSON.parse(msg.content.toString());
-            console.log("Received Email Task:", emailTask);
-            // Process the email task here...
-            channel.ack(msg);
-          } catch (error) {
-            console.error("Failed to process message:", error);
-            channel.nack(msg, false, false);
-          }
-        } else {
-          console.warn("Received null message.");
-        }
-      },
-      { noAck: false }
-    );
+    // const channel = await connectToRabbitMQConsumer();
+    // channel.consume(
+    //   RABBITMQ_QUEUE,
+    //   async (msg: any) => {
+    //     if (msg !== null) {
+    //       try {
+    //         const emailTask = JSON.parse(msg.content.toString());
+    //         console.log("Received Email Task:", emailTask);
+    //         // Process the email task here...
+    //         channel.ack(msg);
+    //       } catch (error) {
+    //         console.error("Failed to process message:", error);
+    //         channel.nack(msg, false, false);
+    //       }
+    //     } else {
+    //       console.warn("Received null message.");
+    //     }
+    //   },
+    //   { noAck: false }
+    // );
     console.log("Waiting for email tasks...");
   } catch (error) {
     console.error("Failed to consume emails:", error);
